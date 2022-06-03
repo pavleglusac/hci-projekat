c15c33775668ee4a0ee950d7b150cf23ad5e1c1a
Dopudj <dejandopudj01@gmail.com>
2022-06-03 03:40:09 +0200

    Add basic model

---
+++ model/Database.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCIProjekat.model
+{
+    internal class Database
+    {
+        public static List<Station> stations { get; set; }
+        public static List<Timetable> timetable { get; set; }
+        public static List<Train> train { get; set; }
+
+        public static void loadData()
+        {
+            stations = new List<Station>();
+            timetable = new List<Timetable>();
+            train = new List<Train>();
+            stations.Add(new Station(new Location(45.246813, 19.853059)));
+            stations.Add(new Station(new Location(46.246813, 19)));
+            stations.Add(new Station(new Location(44.246813, 18)));
+        }
+
+        public static Station addStation(Station station)
+        {
+            stations.Add(station);
+            foreach (Station s in stations)
+            {
+                if (s.location.Equals(station.location))
+                {
+                    return s;
+                }
+            }
+            return station;
+        }
+
+        public static Station getOrAddStation(Location location)
+        {
+            foreach(Station station in stations)
+            {
+                if (station.location.Equals(location))
+                {
+                    return station;
+                }
+            }
+            return addStation(new Station(location));
+
+        }
+    }
+}
---
+++ model/Train.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCIProjekat.model
+{
+    internal class Train
+    {
+        public String name { get; set; }
+        public List<Station> stations { get; set; }
+        public Timetable timetable { get; set; }
+
+        public Train(string name, List<Station> stations, Timetable timetable)
+        {
+            this.name = name;
+            this.stations = stations;
+            this.timetable = timetable;
+        }
+
+        public Train()
+        {
+        }
+
+        public Train(string name, Timetable timetable)
+        {
+            this.name = name;
+            this.timetable = timetable;
+        }
+    }
+}
