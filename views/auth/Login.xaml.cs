5276e3ef5cbddd02334900d60a289e5df9d78154
Milan Sekulic <sekulic.sw54.2019@uns.ac.rs>
2022-06-04 16:24:27 +0200

    Implement basic login field validation and feedback

--- views/auth/Login.xaml.cs
+++ views/auth/Login.xaml.cs
@@ -26,28 +26,58 @@ namespace HCIProjekat.views.auth
         {
             InitializeComponent();
             emailField.Focus();
-
+            DataContext = new LoginInfo("", "");
+            loginError.Visibility = Visibility.Visible;
         }
 
-        private void handleLogin(object sender, RoutedEventArgs e)
+        private void login()
         {
             // login logic
+            hideError();
             string email = emailField.Text;
             string password = passwordField.Password;
             System.Diagnostics.Debug.WriteLine(email);
             System.Diagnostics.Debug.WriteLine(password);
 
+            ((TextBox)emailField).Text = ((TextBox)emailField).Text;
 
             //if successful -> transfer to navigation
             if (email == "test" && password == "test")
             {
                 ShowComponent(new CustomerNavigationLayout());
             }
-            if (email == "admin" && password == "test")
+            else if (email == "admin" && password == "test")
             {
                 ShowComponent(new ManagerNavigationLayout());
             }
+            else
+            {
+                if (emailField.Text.Length > 0)
+                {
+                    loginError.Text = "Pogrešne informacije.";
+                    loginError.Visibility = Visibility.Visible;
+                }
+            }
+        }
 
+        private void handleLoginButton(object sender, RoutedEventArgs e)
+        {
+            login();   
+        }
+
+        private void handleLoginKeypress(object sender, KeyEventArgs e)
+        {
+            hideError();
+            if (e.Key == Key.Return)
+            {
+                login();
+            }
+        }
+        
+        private void hideError()
+        {
+            loginError.Text = "";
+            loginError.Visibility = Visibility.Hidden;
         }
 
         private void handleChooseRegisterButton(object sender, RoutedEventArgs e)
@@ -58,6 +88,16 @@ namespace HCIProjekat.views.auth
         private void ShowComponent(object component)
         {
             NavigationService?.Navigate(component);
+        }
+    }
+    public class LoginInfo
+    {
+        public string Email { get; set; }
+        public string LoginError { get; set; }
+        public LoginInfo(string email, string error)
+        {
+            Email = email;
+            LoginError = error;
         }
     }
 }
---
+++ views/auth/NonEmptyRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HCIProjekat.views.auth
+{
+    internal class NonEmptyRule : ValidationRule
+    {
+        public NonEmptyRule() { }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+
+            System.Diagnostics.Debug.WriteLine(((string)value).Length);
+            if (value == null || ((string)value).Length == 0)
+            {
+                return new ValidationResult(false, "Obavezno polje.");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
