c227d1c9eb18c52406dd3d91f4769e72178c70da
Milan Sekulic <sekulic.sw54.2019@uns.ac.rs>
2022-06-11 21:43:52 +0200

    Add help pages for timetable, ticket history and ticket buying features

--- views/customer/dialogs/SeatChooser.xaml.cs
+++ views/customer/dialogs/SeatChooser.xaml.cs
@@ -126,7 +126,6 @@ namespace HCIProjekat.views.customer.dialogs
                     System.Windows.MessageBox.Show(
                         "Uspešno ste kupili karte!",
                         "Potvrda uspešne kupovine", System.Windows.MessageBoxButton.OK);
-                    Application.Current.Windows[0].Close();
                 }
             }
         }
